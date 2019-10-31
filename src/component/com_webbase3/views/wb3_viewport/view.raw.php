453e2980cbab034e28ebbac883e7548958f3104f
Alexeev-Deomyankov Ilya <45603579+alexeevdemyankov@users.noreply.github.com>
2019-10-31 10:31:53 +0300

    Fix search
    Ctreate uniq for scheme $searchId= 's' . $this->sysVars['wb3_scheme'].'k'.$this->sysVars['wb3_key'].'v'.$this->sysVars['wb3_val'];

--- src/component/com_webbase3/views/wb3_viewport/view.raw.php
+++ src/component/com_webbase3/views/wb3_viewport/view.raw.php
@@ -9,16 +9,17 @@ class Webbase3ViewWb3_Viewport extends JViewLegacy
         $this->sysVars = SysHelper::urlToArray(null, null, null);
         $this->schemeRights = SysHelper::getSchemeRights($this->sysVars['wb3_scheme']);
         if ($this->schemeRights['wb3_r'] == 1) {
+            $searchId= 's' . $this->sysVars['wb3_scheme'].'k'.$this->sysVars['wb3_key'].'v'.$this->sysVars['wb3_val'];
             //Set defaults pagination & order &search
             if ($this->sysVars['wb3_search_clear'] == 1) {
-                SysHelper::clearDefaults('wb3_search' . '_' . $this->sysVars['wb3_scheme']);
-                SysHelper::clearDefaults('wb3_search_field' . '_' . $this->sysVars['wb3_scheme']);
+                SysHelper::clearDefaults('wb3_search' . '_' . $searchId);
+                SysHelper::clearDefaults('wb3_search_field' . '_' . $searchId);
             }
             SysHelper::setDefaults('wb3_onpage', $this->sysVars['wb3_onpage']);
             SysHelper::setDefaults('wb3_order' . '_' . $this->sysVars['wb3_scheme'], $this->sysVars['wb3_order']);
             SysHelper::setDefaults('wb3_direction' . '_' . $this->sysVars['wb3_scheme'], $this->sysVars['wb3_direction']);
-            SysHelper::setDefaults('wb3_search' . '_' . $this->sysVars['wb3_scheme'], $this->sysVars['wb3_search']);
-            SysHelper::setDefaults('wb3_search_field' . '_' . $this->sysVars['wb3_scheme'], $this->sysVars['wb3_search_field']);
+            SysHelper::setDefaults('wb3_search' . '_' . $searchId, $this->sysVars['wb3_search']);
+            SysHelper::setDefaults('wb3_search_field' . '_' . $searchId, $this->sysVars['wb3_search_field']);
             unset($this->sysVars['wb3_onpage']);
             unset($this->sysVars['wb3_order']);
             unset($this->sysVars['wb3_direction']);
@@ -28,8 +29,9 @@ class Webbase3ViewWb3_Viewport extends JViewLegacy
 
             $this->wb3Order = SysHelper::getDefaults('wb3_order' . '_' . $this->sysVars['wb3_scheme']);
             $this->wb3Direction = SysHelper::getDefaults('wb3_direction' . '_' . $this->sysVars['wb3_scheme']);
-            $this->wb3Search = SysHelper::getDefaults('wb3_search' . '_' . $this->sysVars['wb3_scheme']);
-            $this->wb3SearchField = SysHelper::getDefaults('wb3_search_field' . '_' . $this->sysVars['wb3_scheme']);
+            $this->wb3Search = SysHelper::getDefaults('wb3_search' . '_' . $searchId);
+            $this->wb3SearchField = SysHelper::getDefaults('wb3_search_field' . '_' . $searchId);
+
 
             $model = $this->getModel();
             $this->currentScheme = $model->getScheme($this->sysVars);
